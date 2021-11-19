2ebacb354950105a47874bbd8b9a449f5e0474a4
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2021-11-19 19:20:23 +0800

    feat: add navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,32 @@
+<style>
+
+</style>
+
+<nav class="navbar">
+  <div class="container">
+    <div class="title">
+      <a href="/" aria-label="logo">Raven</a>
+    </div>
+    <ul class="items">
+      <li class="item" class:active={segment === "about"}>
+        <a href="/about">About</a>
+      </li>
+      <li class="item" class:active={segment === "projects"}>
+        <a href="/projects">Projects</a>
+      </li>
+      <li class="item" class:active={segment === "skills"}>
+        <a href="/skills">Skills</a>
+      </li>
+      <li class="item" class:active={segment === "contact"}>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+
+</nav>
+
+<script lang="ts">
+
+export let segment: string;
+
+</script>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+  <Navbar {segment} />
+
+<main>
+  <slot></slot>
+</main>
+
+<script lang="ts">
+  import Navbar from "$lib/components/Navbar.svelte"
+
+  export let segment: string = "";
+</script>
