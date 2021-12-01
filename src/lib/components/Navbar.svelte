57cf403376b7109ffeaee4b256a069997634a625
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2021-12-01 22:48:12 +0800

    style: add navbar class

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,26 +1,30 @@
 <style lang="postcss">
 </style>
 
-<nav class="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
-  <div class="navbar-container">
+<nav class="navbar mb-2 shadow-lg bg-primary-focus text-base-300">
     <div class="px-2 mx-2 navbar-start">
-      <span class="text-lg font-bold"> CODERAVEN </span>
+      <span class="text-3xl font-bold"> Raven Rodrigo </span>
     </div>
-    <ul class="flex">
-      <li class ="mr-6" class:active={segment === "about"}>
-        <a class="text-blue-500 hover:text-blue-800" href="/about">About</a>
-      </li>
-      <li class="mr-6" class:active={segment === "project"}>
-        <a class="text-blue-500 hover:text-blue-800" href="/project">Projects</a>
-      </li>
-      <li class="mr-6" class:active={segment === "skills"}>
-        <a class="text-blue-500 hover:text-blue-800" href="/skills">Skills</a>
-      </li>
-      <li class="mr-6" class:active={segment === "contact"}>
-        <a class="text-blue-500 hover:text-blue-800" href="/contact">Contact</a>
-      </li>
-    </ul>
-  </div>
+    <div class="hidden px-2 mx-2 navbar-center lg:flex">
+      <div class="flex items-stretch">
+        <ul class="flex">
+          <li class ="mr-6 text-xl" class:active={segment === "about"}>
+            <a class="btn btn-ghost btn-sm rounded-btn" href="/about">About</a>
+          </li>
+          <li class="mr-6 text-xl" class:active={segment === "project"}>
+            <a class="btn btn-ghost btn-sm rounded-btn" href="/project">Projects</a>
+          </li>
+          <li class="mr-6 text-xl" class:active={segment === "skills"}>
+            <a class="btn btn-ghost btn-sm rounded-btn" href="/skills">Skills</a>
+          </li>
+          <li class="mr-6 text-xl" class:active={segment === "contact"}>
+            <a class="btn btn-ghost btn-sm rounded-btn" href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="navbar-end">
+      </div>
+    </div>
 
 </nav>
 
