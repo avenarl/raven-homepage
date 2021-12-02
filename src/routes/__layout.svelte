365f54184c24b16a494d34e549e285309d23a3a9
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2021-12-02 23:31:09 +0800

    chore: add style to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,20 @@
+<style>
+main {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+div {
+  flex: 1;
+  margin-top: 4.5rem;
+}
+
+:global(html) {
+  scroll-padding-top: 4.5rem;
+}
+</style>
+
 <Navbar {segment} />
 <main>
   <div>
