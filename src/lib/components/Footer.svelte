f1f8be8939e75c0be7701292d920814b5228dc2f
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-03-28 22:56:18 +0800

    feat(footer): add footer date

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,9 +2,9 @@
 
 </style>
 
-<footer class="max-w-4xl mx-auto py-8">
+<footer class="relative mt-12 text-center pt-10 pb-30 md:pb-10 px-1 bg-gray-50 dark:bg-gray-900 border-t border-gray-300 dark:border-gray-800 transition-property-filter ease-out duration-200 font-heading">
 
-<p>Copyright &copy; 2021 Raven</p>
+<p class="leading-relaxed text-slate-600 dark:text-slate-400">Copyright &copy; {new Date().getFullYear()} Raven Rodrigo</p>
 
 </footer>
 
