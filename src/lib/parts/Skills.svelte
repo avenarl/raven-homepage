a73460f16f573c5a8126237f335bc402d1071659
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-03-28 23:03:24 +0800

    style(skills): center title

--- src/lib/parts/Skills.svelte
+++ src/lib/parts/Skills.svelte
@@ -1,4 +1,12 @@
 <style></style>
-<h2>Skills parts folder</h2>
+
+<section class="max-w-[1080px] mx-auto pt-8 px-4 text-center">
+	<h1
+    class="font-heading relative inline-block text-3xl font-semibold text-slate-700 dark:text-slate-200 mb-10 bg-accent)"
+  >
+    Skills
+  </h1>
+
+</section>
 <script></script>
 
