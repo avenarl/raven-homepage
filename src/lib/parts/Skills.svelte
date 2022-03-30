270212eace1ed900b1f0c0ea65faf4bd87f31d14
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-03-30 21:29:59 +0800

    refactor(skills): remove the divider

--- src/lib/parts/Skills.svelte
+++ src/lib/parts/Skills.svelte
@@ -4,13 +4,8 @@
 	<h1
     class="font-heading relative inline-block text-3xl font-semibold text-slate-700 dark:text-slate-200 mb-10 bg-accent)"
   >
-    Skills
+    Skills section
   </h1>
-<div class="flex flex-col border-opacity-50">
-  <div class="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
-  <div class="divider">OR</div>
-  <div class="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
-</div>
 
 </section>
 <script></script>
