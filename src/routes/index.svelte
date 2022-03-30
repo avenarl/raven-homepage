680fed9e8e4ffee070e6c8d2637c3a8ae3249d4d
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-03-30 21:37:30 +0800

    feat(projects): import projects to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,10 +5,11 @@
 <main class="main">
   <Header />
   <Skills />
-
+  <Projects />
 </main>
 
 <script lang="ts">
   import Header from "$lib/parts/Header.svelte";
   import Skills from "$lib/parts/Skills.svelte";
+  import Projects from "$lib/parts/Projects.svelte";
 </script>
