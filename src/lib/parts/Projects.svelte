99538d2278534630dd8cc5e3083ba82a0e969de8
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-03-30 21:34:51 +0800

    feat(projects): add projects section

---
+++ src/lib/parts/Projects.svelte
@@ -0,0 +1,7 @@
+<style></style>
+
+<section>
+	<h1 class="font-heading relative inline-block text-3xl font-semibold text-slate-700 dark:text-slate-200 mb-10 bg-accent">Project Section</h1>
+</section>
+
+<script></script>
