68d882a2cf1e6c48885b368d55b1cecf9815d6e0
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-03-30 21:41:13 +0800

    feat(projects): add text align for title

--- src/lib/parts/Projects.svelte
+++ src/lib/parts/Projects.svelte
@@ -1,7 +1,7 @@
 <style></style>
 
-<section>
-	<h1 class="font-heading relative inline-block text-3xl font-semibold text-slate-700 dark:text-slate-200 mb-10 bg-accent">Project Section</h1>
+<section class="max-w-[1080px] mx-auto pt-8 px-4 text-center">
+	<h1 class="font-heading relative inline-block text-3xl font-semibold text-slate-700 dark:text-slate-200 mb-10">Project Section</h1>
 </section>
 
 <script></script>
